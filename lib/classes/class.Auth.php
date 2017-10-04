a7e9df261d57e2dcf9f5b545dbba14aa54d8a6b5
theflu <theflu@users.noreply.github.com>
2017-10-04 11:01:31 -0400

    Assing user type at login

--- lib/classes/class.Auth.php
+++ lib/classes/class.Auth.php
@@ -20,7 +20,7 @@ class Auth {
 	public function login($username, $password, $redirect = true) {
 		if (isset($_SESSION['config']['users'][$username]) && password_verify($password, $_SESSION['config']['users'][$username]['password'])) {
 			$_SESSION['username'] = $username;
-			$_SESSION['user_type'] = $_SESSION['users'][$username]['type'];
+			$_SESSION['user_type'] = $_SESSION['config']['users'][$username]['type'];
 			if ($redirect) $this->redirect();
 			
 			return true;
