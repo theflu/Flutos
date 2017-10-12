20888cc5fea1da10981d09e198ace85777616d00
theflu <theflu@users.noreply.github.com>
2017-10-12 19:12:08 -0400

    testing

--- lib/routes.php
+++ lib/routes.php
@@ -48,7 +48,7 @@ $router->get('/album/{album_slug}/delete', function ($album_slug, $image) {
 
 $router->get('/album/{album_slug}/{image}', function ($album_slug, $image) use ($twig) {
     $album_class = new Album;
-
+    echo 'here';
     $album_class->showImage($album_slug, $image);
 });
 
