d8ce8af706ac2aee747ed0414402eea1f97f9935
theflu <theflu@users.noreply.github.com>
2017-10-26 22:36:52 -0400

    Adding 404

--- lib/routes.php
+++ lib/routes.php
@@ -482,3 +482,12 @@ $router->post('/setup', function () use ($twig) {
 
     echo $twig->render('setup.twig', $vars);
 });
+
+
+//
+// Show albums
+//
+
+$router->set404(function () use ($twig) {
+    echo $twig->render('404.twig');
+});
